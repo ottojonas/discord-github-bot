5a6db1aeeb713d6451f54c04bac5802642f25683
ottojonas <ottobjonas@icloud.com>
2025-01-13 22:23:38 +0000

    testing deployment

--- index.js
+++ index.js
@@ -65,6 +65,6 @@ function sendMessageToDiscord(message) {
   }
 }
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(4040, () => {
+  console.log("Server is running on port 4040");
 });
