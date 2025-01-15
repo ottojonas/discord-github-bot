aa4c09846d8bdaffe2c7077ce628df5b555870bd
ottojonas <ottobjonas@outlook.com>
2025-01-15 11:27:48 +0000

    connecting

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { Client, Intents } from "discord.js";
+import pkg from "discord.js";
+import dotenv from "dotenv";
+import crypto from "crypto";
+dotenv.config();
+
+const { Client, GatewayIntentBits } = pkg;
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 const discordToken = process.env.DISCORD_TOKEN;
 const discordChannelId = process.env.DISCORD_CHANNEL_ID;
+const githubWebhookSecret = process.env.GITHUB_WEBHOOK_SECRET;
 
-const client = new Client({ intents: [] });
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
 
 client.once("ready", () => {
   console.log("bot is ready");
@@ -16,7 +24,35 @@ client.once("ready", () => {
 
 client.login(discordToken);
 
-app.use(bodyParser.json());
+app.use(
+  bodyParser.json({
+    verify: (req, res, buf) => {
+      req.rawBody = buf.toString();
+    },
+  })
+);
+
+// Middleware to verify GitHub webhook signature
+app.use((req, res, next) => {
+  const signature = req.headers["x-hub-signature-256"];
+  if (!signature) {
+    console.log("No signature found");
+    return res.status(401).send("No signature found");
+  }
+
+  const hmac = crypto.createHmac("sha256", githubWebhookSecret);
+  const digest = `sha256=${hmac.update(req.rawBody).digest("hex")}`;
+
+  console.log(`Expected: ${digest}`);
+  console.log(`Received: ${signature}`);
+
+  if (signature !== digest) {
+    console.log("Invalid signature");
+    return res.status(401).send("Invalid signature");
+  }
+
+  next();
+});
 
 app.post("/webhook", (req, res) => {
   const event = req.headers["x-github-event"];
